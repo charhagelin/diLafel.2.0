b106106c6b8ffb063a22d48afb52fa288a42d336
charhagelin <charhagelin@gmail.com>
2018-05-16 14:28:24 +0200

    adding some styling to inventory + add new item form

--- src/Components/AddNewPage.js
+++ src/Components/AddNewPage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import AddItemForm from './AddItemForm';
+import { Col, Row } from 'react-bootstrap';
 import '../Styles/AddNewPage.css'
 
 class AddNewPage extends Component {
@@ -23,54 +24,71 @@ class AddNewPage extends Component {
         const item = this.props.items[key];
 
         return (
-            <div key={key}>
-            <form className="inventory-form" >
-            <input className="edit-item-form"
-                value={item.name}
-                name= "name"
-                onChange={(e) => this.handleChange(e, key)}
-                type="text"
-                placeholder="name"
-                 />
-            <textarea className="edit-item-form-textarea"
-                value={item.desc}
-                name= "desc"
-                onChange={(e) => this.handleChange(e, key)}
-                type="textarea"
-                placeholder="Description"
-                 >
-            </textarea >
-            <input className="edit-item-form"
-                value={item.price}
-                name= "price"
-                onChange={(e) => this.handleChange(e, key)}
-                type="number"
-                placeholder="Price" 
-                 />
-            <input className="edit-item-form"
-                value={item.tags}
-                name= "tags"
-                onChange={(e) => this.handleChange(e, key)}
-                type="text"
-                placeholder="Tags" />
-            <select className="edit-item-form"
-                value={item.unit}
-                name= "unit"
-                onChange={(e) => this.handleChange(e, key)} 
-                placeholder="select"
-                 >
-                <option>...</option>
-                <option value="Kg">Kg</option>
-                <option value="Box">Box</option>
-            </select>
-            <input className="edit-item-form"
-                value={item.image}
-                name= "image"
-                onChange={(e) => this.handleChange(e, key)}
-                type="text"
-                placeholder="image URL"
-                 />
-        </form>
+            <div key={key} className="inventory-form-container">
+                <form className="inventory-form" >
+                    <Row>
+                        <Col xs={6} md={4}>
+                        <input className="edit-item-form"
+                            label="name"
+                            value={item.name}
+                            name= "name"
+                            onChange={(e) => this.handleChange(e, key)}
+                            type="text"
+                            placeholder="name"
+                            />
+                        </Col>
+                        <Col xs={6} md={4}>     
+                            <input className="edit-item-form"
+                                value={item.price}
+                                name= "price"
+                                onChange={(e) => this.handleChange(e, key)}
+                                type="number"
+                                placeholder="Price" 
+                            />
+                        </Col>
+                        <Col xs={6} md={4}>      
+                            <select className="edit-item-form"
+                                value={item.unit}
+                                name= "unit"
+                                onChange={(e) => this.handleChange(e, key)} 
+                                placeholder="select"
+                            >
+                            <option>...</option>
+                            <option value="Kg">Kg</option>
+                            <option value="Box">Box</option>
+                        </select>
+                        </Col>     
+                    </Row>        
+                    <textarea className="edit-item-form-textarea"
+                        value={item.desc}
+                        name= "desc"
+                        onChange={(e) => this.handleChange(e, key)}
+                        type="textarea"
+                        placeholder="Description"
+                        >
+                    </textarea >
+                  <Row>
+                    <Col sm={12} md={6}>
+                        <input className="edit-item-form"
+                        value={item.tags}
+                        name= "tags"
+                        onChange={(e) => this.handleChange(e, key)}
+                        type="text"
+                        placeholder="Tags" />
+                    </Col>    
+                    <Col sm={12} md={6}>
+                        <input className="edit-item-form"
+                        value={item.image}
+                        name= "image"
+                        onChange={(e) => this.handleChange(e, key)}
+                        type="text"
+                        placeholder="image URL"
+                        />
+                    </Col>    
+                 </Row>
+                 <hr />
+                </form>
+                <hr />
             </div>
         ) 
     }
@@ -81,8 +99,9 @@ class AddNewPage extends Component {
                 <AddItemForm 
                     addNewItem={this.props.addNewItem}
                 />
+                <h2>Inventory</h2>
                 {Object.keys(this.props.items).map(this.renderInventory)}
-
+              
             </div>
         )
     }
