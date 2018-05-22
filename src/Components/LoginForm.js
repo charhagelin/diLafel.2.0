1fce92bd2f3fbd0b700887f2a34155d31573596b
charhagelin <charhagelin@gmail.com>
2018-05-22 09:17:38 +0200

    fixed bug in log in function

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import '../Styles/LoginForm.css';
 import { app, base } from '../base';
 import { Redirect } from 'react-router-dom';
-import { Grid, Col, Row, Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { Col, Row, Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 
 
 
@@ -49,17 +49,15 @@ class LoginForm extends Component {
             return <Redirect to="/" />
         }
         return(
-            <Grid>
-                <Col xs={6} md={4}>
-                <Form className="login-form" onSubmit={(event) => this.authWithEmailPassword(event)} ref={(form) => {this.loginForm = form}} > 
+            <div>
+                <form className="login-form" onSubmit={(event) => this.authWithEmailPassword(event)} ref={(form) => {this.loginForm = form}} > 
                 <h5>Note:</h5>
                     If you dont't already have an account this form will create one for you.
-                    <FormControl type="email" placeholder="email" name="email" ref={(input) => {this.emailInput = input}}/>
-                    <FormControl type="password" placeholder="password" name="password" ref={(input) => {this.passwordInput = input}}/>
-                    <Button type="submit" value="sign in" className="btn-login">Log In</Button>
-                </Form>
-                </Col>
-            </Grid>
+                    <input type="email" placeholder="email" name="email" ref={(input) => {this.emailInput = input}}/>
+                    <input type="password" placeholder="password" name="password" ref={(input) => {this.passwordInput = input}}/>
+                    <Button type="submit" value="sign in">Log In</Button>
+                </form>
+            </div>
     )
 }
 }
